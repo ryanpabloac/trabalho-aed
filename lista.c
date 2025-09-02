26b0b00f5ab7b5dec8a123767b0e00224cbbdb97
pedrosalesufu <pedrosales.ufu@gmail.com>
2025-09-02 11:21:24 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -6,8 +6,50 @@ void criarListaConsulta(ListaConsulta *L) {
     L->tamanho = 0;
 }
 
+void insereConsultaNoFinal(ListaConsulta *L, Consulta consulta) {
+    NoConsulta *novoNo = (NoConsulta*) malloc(sizeof(NoConsulta));
+    if (novoNo == NULL) {
+        printf("Erro de alocacao de memoria\n");
+        return;
+    }
+    novoNo->dados = consulta;
+    novoNo->prox = NULL;
+
+    if (L->cabeca == NULL) {
+        novoNo->ant = NULL;
+        L->cabeca = novoNo;
+        L->cauda = novoNo;
+    } else {
+        novoNo->ant = L->cauda;
+        L->cauda->prox = novoNo;
+        L->cauda = novoNo;
+    }
+    L->tamanho++;
+}
+
 void criarListaMedicamento(ListaMedicamento *L) {
     L->cabeca = NULL;
     L->cauda = NULL;
     L->tamanho = 0;
+}
+
+void insereMedicamentoNoFinal(ListaMedicamento *L, Medicamento med) {
+    NoMedicamento *novoNo = (NoMedicamento*) malloc(sizeof(NoMedicamento));
+    if (novoNo == NULL) {
+        printf("Erro de alocacao de memoria\n");
+        return;
+    }
+    novoNo->dados = med;
+    novoNo->prox = NULL;
+
+    if (L->cabeca == NULL) {
+        novoNo->ant = NULL;
+        L->cabeca = novoNo;
+        L->cauda = novoNo;
+    } else {
+        novoNo->ant = L->cauda;
+        L->cauda->prox = novoNo;
+        L->cauda = novoNo;
+    }
+    L->tamanho++;
 }
