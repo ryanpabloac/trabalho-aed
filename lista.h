e249522d257463c624b2b6318ee31d095097c17f
Ryan Costa <ryanpac16@gmail.com>
2025-09-01 23:15:42 -0300

    correcoes TAD lista

--- lista.h
+++ lista.h
@@ -1,6 +1,9 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 typedef  unsigned int uint;
 
 typedef struct descritorConsulta ListaConsulta;
@@ -8,30 +11,28 @@ typedef struct noConsulta NoConsulta;
 typedef struct descritorMedicamento ListaMedicamento;
 typedef struct noMedicamento NoMedicamento;
 
+struct descritorMedicamento {
+    NoMedicamento *cabeca;
+    NoMedicamento *cauda;
+    uint tamanho;
+};
+struct noMedicamento {
+    NoMedicamento *ant;
+    ListaMedicamento dados;
+    NoMedicamento *prox;
+};
 struct descritorConsulta {
     NoConsulta *cabeca;
     NoConsulta *cauda;
     uint tamanho;
 };
-
 struct noConsulta {
     NoConsulta *ant;
     ListaMedicamento dados;
     NoConsulta *prox;
 };
 
-struct descritorMedicamento {
-    NoMedicamento *cabeca;
-    NoMedicamento *cauda;
-    uint tamanho;
-};
-
-struct noMedicamento {
-    NoMedicamento *ant;
-    ListaMedicamento dados;
-    NoMedicamento *prox;
-};
-
 void criarListaConsulta(ListaConsulta *);
 void criarListaMedicamento(ListaMedicamento *);
+
 #endif
--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-int main() {
+#include "lista.h"
 
+int main() {
+    ListaConsulta lista;
+    criarListaConsulta(&lista);
+
+    system("pause");
     return 0;
 }
